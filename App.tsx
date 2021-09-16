f6cd061cc222b268493bb2204d210bdb06303d41
Raouf Madani <raoufmadani@iMac-de-Raouf.local>
2021-09-16 12:35:53 +0100

    install redux to manage the store

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './navigation/Navigator';
 import {enableScreens} from 'react-native-screens';
-import AppLoading from 'expo-app-loading';
+
 
 enableScreens();
 
